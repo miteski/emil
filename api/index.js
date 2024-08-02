fe83acd225399cb3b1aef599b9e1423f62e3d4b1
miteski <miteski.stefan@gmail.com>
2024-08-02 07:54:07 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mysql = require('mysql2/promise');
-const app = express();
+const multer = require('multer');
+const { parse } = require('csv-parse');
 
 app.use(express.json());
 
