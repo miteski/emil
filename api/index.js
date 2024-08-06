e41cf1f249278497452054e7895412527975547a
miteski <miteski.stefan@gmail.com>
2024-08-06 11:22:03 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,3 +1,8 @@
+console.log('Environment variables:');
+console.log('MYSQL_HOST:', process.env.MYSQL_HOST);
+console.log('MYSQL_USER:', process.env.MYSQL_USER);
+console.log('MYSQL_DATABASE:', process.env.MYSQL_DATABASE);
+
 const express = require('express');
 const mysql = require('mysql2/promise');
 const multer = require('multer');
