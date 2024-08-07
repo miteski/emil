f15a2635f9a2d7224c50dff1176aa92f75e07407
miteski <miteski.stefan@gmail.com>
2024-08-07 19:37:41 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -388,5 +388,9 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 
 module.exports = app;
