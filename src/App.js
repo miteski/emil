feb901a9be475ab3740d297e19b931c949a345cc
miteski <miteski.stefan@gmail.com>
2024-08-10 12:03:38 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import ViewAgents2 from './components/ViewAgents2';
+import Layout from './components/Layout';
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/view-agents2" component={ViewAgents2} />
-        {/* Other routes */}
-      </Switch>
+      <Layout>
+        <Switch>
+          <Route exact path="/" render={() => <Redirect to="/view-agents2" />} />
+          <Route path="/view-agents2" component={ViewAgents2} />
+          {/* Add other routes here as needed */}
+          <Route path="*" render={() => <div>Page not found</div>} />
+        </Switch>
+      </Layout>
     </Router>
   );
 }
