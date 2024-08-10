601ba291228842122ee761feff03ba785619c85b
miteski <miteski.stefan@gmail.com>
2024-08-10 13:15:51 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+console.log('React script is running');
 
+try {
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} catch (error) {
+  console.error('Error rendering React app:', error);
+  document.getElementById('root').innerHTML += '<p>Error rendering React app: ' + error.message + '</p>';
+}
