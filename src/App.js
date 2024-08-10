525c82aa8767bc5801a74e3180f7f9d3a0723e0f
miteski <miteski.stefan@gmail.com>
2024-08-10 11:41:14 +0200

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ViewAgents2 from './components/ViewAgents2';
+
+function App() {
+  return (
+    <div className="App">
+      <ViewAgents2 />
+    </div>
+  );
+}
+
+export default App;
