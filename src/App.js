b341e37f6f86c4ee0b59079bedcc0eb8c16291df
miteski <miteski.stefan@gmail.com>
2024-08-10 13:16:13 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import ViewAgents2 from './components/ViewAgents2';
-import Layout from './components/Layout';
 
 function App() {
   return (
-    <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Navigate to="/view-agents2" replace />} />
-          <Route path="/view-agents2" element={<ViewAgents2 />} />
-          <Route path="*" element={<div>Page not found</div>} />
-        </Routes>
-      </Layout>
-    </Router>
+    <div className="App">
+      <h1>EMIL Insurance Suite</h1>
+      <ViewAgents2 />
+    </div>
   );
 }
 
