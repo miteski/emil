a636efb0d9be29bded7ca98d2eda76a5e0e59ef0
miteski <miteski.stefan@gmail.com>
2024-08-10 16:03:50 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import ViewAgents2 from './components/ViewAgents2';
 
 function App() {
+  console.log('App component rendered');
   return (
     <Router>
-      <div className="App">
+      <div className="App" style={{minHeight: '100vh', background: 'white'}}>
         <Routes>
           <Route path="/view-agents2" element={<ViewAgents2 />} />
           <Route path="/" element={<Navigate to="/view-agents2" replace />} />
